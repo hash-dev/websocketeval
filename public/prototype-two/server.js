5abc9a00d0f86f453c337928467e96957700981b
hash-developer <kontakt@hash-developer.de>
2015-12-04 18:22:47 +0100

    Added node-statsd first metric to project

--- public/prototype-two/server.js
+++ public/prototype-two/server.js
@@ -1,12 +1,17 @@
+// Get public IP-address
 var getIP = require('external-ip')();
- 
+
 getIP(function (err, ip) {
     if (err) {
-        // every service in the list has failed 
+        // every service in the list has failed
         throw err;
     }
     console.log(ip);
 });
+
+// Initialize StatsD module
+var StatsD = require('node-statsd'),
+      client = new StatsD();
 
 var WebSocketServer = require('ws').Server,
     wss = new WebSocketServer({ port: 8081 });
@@ -16,6 +21,19 @@ wss.on('connection', function connection(ws) {
     console.log('received: %s', message);
   });
 
+  // On every connection increment the StatsD-connection-value
+  client.increment('client-connection');
+
+  ws.on('open', function open() {
+        console.log('ws connection open');
+  });
+
+  ws.on('close', function close() {
+        console.log('ws connection closed');
+        // client.decrement('client-connection');
+  });
+
+  // Send a random number every second as long as the connection persists
   setInterval(function() {
         ws.send(String(getRandomNumber()));
     }, 1000);
